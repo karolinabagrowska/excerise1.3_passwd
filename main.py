c892a2600abf938d8dd74fafb8650ccb69483dc0
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-01 21:26:55 +0200

    fix password

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def password_get(response: Response, password = None, password_hash = None):
     if password_without_spaces == "":
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response.status_code
-    if password_without_spaces is None or password_hash is None:
+    if password is None or password_hash is None:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response.status_code
-    password_sha512 = hashlib.sha512(password_without_spaces.encode('utf-8')).hexdigest()
+    password_sha512 = hashlib.sha512(password.encode('utf-8')).hexdigest()
     if password_sha512 == password_hash:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response.status_code
