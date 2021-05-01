9c5d88376eba0f388c78ee06d4a4ca18e01bae1b
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-01 21:17:10 +0200

    remove whitespaces

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import hashlib
 app = FastAPI()
 
 
-@app.get("/auth", status_code = 200)
+@app.get("/auth")
 def password_get(response: Response, password = None, password_hash = None):
     #password_hash = hashlib.sha512(("haslo").encode('utf-8'))
     #print(password_hash.hexdigest())
     #password_hash = hashlib.sha512(password.encode())
-    if password is None or password_hash is None:
+    password_without_spaces = password.replace(" ", "")
+    if password_without_spaces is None or password_hash is None:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response.status_code
-    password_sha512 = hashlib.sha512(password.encode('utf-8')).hexdigest()
+    password_sha512 = hashlib.sha512(password_without_spaces.encode('utf-8')).hexdigest()
     if password_sha512 == password_hash:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response.status_code
