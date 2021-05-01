43731fcff7887400828527259ac2d34f477e4574
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-01 21:05:49 +0200

    empty parameters

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ app = FastAPI()
 
 
 @app.get("/auth", status_code = 200)
-def password_get(password, password_hash, response: Response):
+def password_get(response: Response, password = None, password_hash = None):
     #password_hash = hashlib.sha512(("haslo").encode('utf-8'))
     #print(password_hash.hexdigest())
     #password_hash = hashlib.sha512(password.encode())
+    if password is None or password_hash is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return response.status_code
     password_sha512 = hashlib.sha512(password.encode('utf-8')).hexdigest()
     if password_sha512 == password_hash:
         response.status_code = status.HTTP_204_NO_CONTENT
