2d4561dcf4d2fa3e55851440c08ec398c292b445
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-01 20:42:57 +0200

    password sha512 definition

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+import hashlib
+app = FastAPI()
+
+
+@app.get("/auth", status_code = 200)
+def password_get(password, password_hash, response: Response):
+    #password_hash = hashlib.sha512(("haslo").encode('utf-8'))
+    #print(password_hash.hexdigest())
+    #password_hash = hashlib.sha512(password.encode())
+    password_sha512 = hashlib.sha512(password.encode('utf-8')).hexdigest()
+    if password_sha512 == password_hash:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response.status_code
+    elif password_sha512 != password_hash:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return response.status_code
